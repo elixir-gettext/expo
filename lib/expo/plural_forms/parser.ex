be0824b969224cae486a3f0bc64a0ba2aaff913e
Jonatan Männchen <jonatan@maennchen.ch>
2022-04-01 19:35:15 +0100

    Add known plural forms (#22)

---
+++ lib/expo/plural_forms/parser.ex
@@ -0,0 +1,237 @@
+# credo:disable-for-this-file Credo.Check.Refactor.PipeChainStart
+defmodule Expo.PluralForms.Parser do
+  @moduledoc false
+
+  # This parses is extrapolated from prior work of Stefan Lapels:
+  # https://github.com/slapers/ex_sel
+
+  import NimbleParsec
+
+  alias Expo.PluralForms
+
+  #
+  # Basics
+  #
+
+  optional_whitespace =
+    ascii_char([?\s, ?\n, ?\r, ?\t])
+    |> times(min: 0)
+    |> label("whitespace")
+    |> ignore()
+
+  ignore_surrounding_whitespace = fn p ->
+    optional_whitespace
+    |> concat(p)
+    |> ignore(optional_whitespace)
+  end
+
+  lparen = label(ascii_char([?(]), "(")
+  rparen = label(ascii_char([?)]), ")")
+
+  gt = string(">") |> replace(:>) |> label(">")
+  gte = string(">=") |> replace(:>=) |> label(">=")
+  lt = string("<") |> replace(:<) |> label("<")
+  lte = string("<=") |> replace(:<=) |> label("<=")
+  eq = string("==") |> replace(:==) |> label("==")
+  neq = string("!=") |> replace(:!=) |> label("!=")
+
+  mod = string("%") |> replace(:%) |> label("%")
+
+  and_ = "&&" |> string() |> replace(:&&)
+  or_ = "||" |> string |> replace(:||)
+
+  #
+  # Value Expressions
+  #
+  # <value_expression_num> ::= <int>
+  # <int>                  ::= <digit>
+  # <value_expression_var> ::= "n"
+  # <digit>                ::= "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
+
+  value_expression_num = label(integer(min: 1), "integer")
+
+  value_expression_var =
+    ascii_char([?n])
+    |> replace(:n)
+    |> label("n")
+
+  value_expression = choice([value_expression_num, value_expression_var])
+
+  #
+  # Arithmetic expressions
+  #
+  # <arithmetic_expression>        ::= <term> {% <term>}
+  # <term>                         ::= <arithmetic_expression_factor> {* | / <arithmetic_expression_factor>}
+  # <arithmetic_expression_factor> ::= ( <arithmetic_expression> ) | <const>
+
+  arithmetic_expression_factor =
+    [
+      ignore(lparen) |> parsec(:arithmetic_expression) |> ignore(rparen),
+      value_expression_num,
+      value_expression_var
+    ]
+    |> choice()
+    |> ignore_surrounding_whitespace.()
+
+  defcombinatorp :arithmetic_expression,
+                 arithmetic_expression_factor
+                 |> repeat(concat(mod, arithmetic_expression_factor))
+                 |> reduce(:fold_infixl)
+
+  #
+  # Comparison expressions
+  #
+  # <comparison_expression>        ::= <comparison_expression_factor> <eq_op> <comparison_expression_factor> | <comparison_expression_term>
+  # <comparison_expression_term>   ::= (<comparison_expression>) | <comparison_expression_ord>
+  # <comparison_expression_factor> ::= (<bexpr>) | <comparison_expression_term> | <arithmetic_expression> | <value_expression>
+  # <comparison_expression_ord>    ::= <arithmetic_expression> <ord_op> <arithmetic_expression>
+  # <ord_op>                       ::= > | >= | < | <=
+  # <eq_op>                        ::= != | ==
+
+  comparison_expression_ord =
+    parsec(:arithmetic_expression)
+    |> choice([gte, lte, gt, lt])
+    |> parsec(:arithmetic_expression)
+    |> reduce(:fold_infixl)
+
+  comparison_expression_term =
+    [
+      ignore(lparen) |> parsec(:comparison_expression) |> ignore(rparen),
+      comparison_expression_ord
+    ]
+    |> choice()
+    |> ignore_surrounding_whitespace.()
+
+  comparison_expression_factor =
+    [
+      ignore(lparen) |> parsec(:boolean_expression) |> ignore(rparen),
+      comparison_expression_term,
+      parsec(:arithmetic_expression),
+      value_expression
+    ]
+    |> choice()
+    |> ignore_surrounding_whitespace.()
+
+  defcombinatorp :comparison_expression,
+                 choice([
+                   comparison_expression_factor
+                   |> choice([eq, neq])
+                   |> concat(comparison_expression_factor)
+                   |> reduce(:fold_infixl),
+                   comparison_expression_term
+                 ])
+
+  #
+  # Boolean logic expressions
+  #
+  # Priority order (high to low):  AND, OR
+  # expressions in parens are evaluated first
+  #
+  # <boolean_expression>        ::= <boolean_expression_term> {<or> <boolean_expression_term>}
+  # <boolean_expression_term>   ::= <boolean_expression_factor> {<and> <boolean_expression_factor>}
+  # <boolean_expression_factor> ::= ( <boolean_expression> ) | <comparison_expression> | <vexpr_bool>
+  # <or>                        ::= '||'
+  # <and>                       ::= '&&'
+
+  boolean_expression_factor =
+    choice([
+      ignore(lparen) |> parsec(:boolean_expression) |> ignore(rparen),
+      parsec(:comparison_expression),
+      value_expression_num
+    ])
+    |> ignore_surrounding_whitespace.()
+    |> label("logic factor")
+
+  boolean_expression_term =
+    boolean_expression_factor
+    |> repeat(concat(and_, boolean_expression_factor))
+    |> reduce(:fold_infixl)
+    |> label("logic term")
+
+  defcombinatorp :boolean_expression,
+                 boolean_expression_term
+                 |> repeat(concat(or_, boolean_expression_term))
+                 |> reduce(:fold_infixl)
+                 |> label("boolean logic expression")
+
+  #
+  # Conditional Expression
+  #
+  # <conditional_expression ::= <boolean_expression> "?" <conditional_expression> ":" <conditional_expression> | <boolean_expression>
+
+  defcombinatorp :conditional_expression,
+                 parsec(:boolean_expression)
+                 |> optional(
+                   string("?")
+                   |> label("?")
+                   |> replace(:if)
+                   |> concat(parsec(:conditional_expression))
+                   |> concat(string(":") |> label(":") |> ignore())
+                   |> concat(parsec(:conditional_expression))
+                 )
+                 |> reduce(:to_conditional)
+
+  #
+  # Full Header
+  #
+  # <nplurals>     ::= "nplurals=" <digit>
+  # <plural>       ::= "plural=" <conditional_expression>
+  # <plural_forms> ::= <nplurals> ";" <plural> ";" EOS
+
+  nplurals =
+    string("nplurals=")
+    |> ignore()
+    |> integer(min: 1)
+
+  plural =
+    string("plural=")
+    |> ignore()
+    |> concat(
+      choice([
+        parsec(:conditional_expression),
+        ignore(lparen) |> parsec(:conditional_expression) |> ignore(rparen)
+      ])
+    )
+
+  defparsecp :plural_forms,
+             nplurals
+             |> concat(optional_whitespace)
+             |> concat(ignore(string(";")))
+             |> concat(optional_whitespace)
+             |> concat(plural)
+             |> concat(optional_whitespace)
+             |> concat(ignore(string(";")))
+             |> concat(optional_whitespace)
+             |> eos()
+             |> reduce({List, :to_tuple, []})
+
+  @spec parse(content :: String.t()) ::
+          {:ok, PluralForms.t()}
+          | {:error,
+             {:parse_error, message :: String.t(), line :: pos_integer(), offset :: pos_integer()}}
+  def parse(content) do
+    case plural_forms(content) do
+      {:ok, [result], "", _context, _line, _offset} ->
+        {:ok, result}
+
+      {:error, message, _rest, _context, {line, _line_offset}, offset} ->
+        {:error, {:parse_error, message, line, offset}}
+    end
+  end
+
+  defp fold_infixl(acc) do
+    acc
+    |> Enum.reverse()
+    |> Enum.chunk_every(2)
+    |> List.foldr([], fn
+      [l], [] -> l
+      [r, op], l -> {op, l, r}
+    end)
+  end
+
+  defp to_conditional(tokens)
+  defp to_conditional([expr]), do: expr
+
+  defp to_conditional([condition, :if, truthy, falsy]),
+    do: {:if, condition, truthy, falsy}
+end
--- test/expo/plural_forms_test.exs
+++ test/expo/plural_forms_test.exs
@@ -272,9 +272,16 @@ defmodule Expo.PluralFormsTest do
                      : n%100>=11 ? 4 : 5;
                """)
     end
+
+    test "invalid expression" do
+      assert {:error, {:parse_error, _message, 1, 19}} =
+               PluralForms.parse("nplurals=6; plural=m % 20 == 1;")
+    end
   end
 
   describe "index/2" do
+    # Tests copied from https://github.com/elixir-gettext/gettext/blob/600e4630fb7db514d464f92e2069a138cf9c68a1/test/gettext/plural_test.exs#L1
+
     test "identity" do
       assert 2 = PluralForms.index(:n, 2)
       assert 7 = PluralForms.index(7, 2)
@@ -340,5 +347,148 @@ defmodule Expo.PluralFormsTest do
     assert PluralForms.index(plurals, 2011) == 4
     assert PluralForms.index(plurals, 2099) == 4
     assert PluralForms.index(plurals, 3000) == 5
+  end
+
+  describe "plural_form/1" do
+    test "x_* locales are pluralized like x except for exceptions" do
+      assert nplurals("en") == nplurals("en_GB")
+
+      assert plural("pt", 0) == 1
+      assert plural("pt", 1) == 0
+      assert plural("pt_BR", 0) == 0
+      assert plural("pt_BR", 1) == 0
+    end
+
+    test "locale with a territory" do
+      # The _XX in en_XX gets stripped and en_XX is pluralized as en.
+      assert nplurals("en_XX") == nplurals("en")
+      assert plural("en_XX", 100) == plural("en", 100)
+    end
+
+    test "unknown locale" do
+      assert :error = PluralForms.plural_form("wat")
+
+      # This happens with dash as the territory/locale separator
+      # (https://en.wikipedia.org/wiki/IETF_language_tag).
+      assert :error = PluralForms.plural_form("en-us")
+    end
+
+    test "locales with one form" do
+      assert nplurals("ja") == 1
+      assert plural("ja", 0) == 0
+      assert plural("ja", 8) == 0
+    end
+
+    test "locales with two forms where 0 is same as > 1" do
+      assert nplurals("it") == 2
+      assert plural("it", 1) == 0
+      assert plural("it", 0) == 1
+      assert plural("it", 13) == 1
+    end
+
+    test "locales with two forms where 0 and 1 are the same" do
+      assert nplurals("fr") == 2
+      assert plural("fr", 0) == 0
+      assert plural("fr", 1) == 0
+      assert plural("fr", 2) == 1
+    end
+
+    test "locales that belong to the 3-forms slavic family" do
+      assert nplurals("ru") == 3
+      assert plural("ru", 21) == 0
+      assert plural("ru", 42) == 1
+      assert plural("ru", 11) == 2
+    end
+
+    test "locales that belong to the alternative 3-forms slavic family" do
+      assert nplurals("cs") == 3
+      assert plural("cs", 1) == 0
+      assert plural("cs", 3) == 1
+      assert plural("cs", 12) == 2
+    end
+
+    test "locales that don't belong to any pluralization family" do
+      assert plural("ar", 0) == 0
+      assert plural("ar", 1) == 1
+      assert plural("ar", 2) == 2
+      assert plural("ar", 505) == 3
+      assert plural("ar", 733) == 4
+      assert plural("ar", 101) == 5
+
+      assert plural("csb", 1) == 0
+      assert plural("csb", 33) == 1
+      assert plural("csb", 115) == 2
+
+      assert plural("cy", 1) == 0
+      assert plural("cy", 2) == 1
+      assert plural("cy", 23) == 2
+      assert plural("cy", 8) == 3
+
+      assert plural("ga", 1) == 0
+      assert plural("ga", 2) == 1
+      assert plural("ga", 4) == 2
+      assert plural("ga", 10) == 3
+      assert plural("ga", 133) == 4
+
+      assert plural("gd", 1) == 0
+      assert plural("gd", 12) == 1
+      assert plural("gd", 18) == 2
+      assert plural("gd", 20) == 3
+
+      assert plural("is", 71) == 0
+      assert plural("is", 11) == 1
+
+      assert plural("jv", 0) == 0
+      assert plural("jv", 13) == 1
+
+      assert plural("kw", 1) == 0
+      assert plural("kw", 2) == 1
+      assert plural("kw", 3) == 2
+      assert plural("kw", 99) == 3
+
+      assert plural("lt", 81) == 0
+      assert plural("lt", 872) == 1
+      assert plural("lt", 112) == 2
+
+      assert plural("lv", 31) == 0
+      assert plural("lv", 9) == 1
+      assert plural("lv", 0) == 2
+
+      assert plural("mk", 131) == 0
+      assert plural("mk", 132) == 1
+      assert plural("mk", 9) == 2
+
+      assert plural("mnk", 0) == 0
+      assert plural("mnk", 1) == 1
+      assert plural("mnk", 12) == 2
+
+      assert plural("mt", 1) == 0
+      assert plural("mt", 0) == 1
+      assert plural("mt", 119) == 2
+      assert plural("mt", 67) == 3
+
+      assert plural("pl", 1) == 0
+      assert plural("pl", 102) == 1
+      assert plural("pl", 713) == 2
+
+      assert plural("ro", 1) == 0
+      assert plural("ro", 19) == 1
+      assert plural("ro", 80) == 2
+
+      assert plural("sl", 320) == 0
+      assert plural("sl", 101) == 1
+      assert plural("sl", 202) == 2
+      assert plural("sl", 303) == 3
+    end
+  end
+
+  defp plural(code, n) do
+    {:ok, {_nplurals, plural_form}} = PluralForms.plural_form(code)
+    PluralForms.index(plural_form, n)
+  end
+
+  defp nplurals(code) do
+    {:ok, {nplurals, _plural_form}} = PluralForms.plural_form(code)
+    nplurals
   end
 end
