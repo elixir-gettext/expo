2f3267e2cc68f353be99627176e87a7daef86f83
Jonatan Männchen <jonatan@maennchen.ch>
2022-04-06 00:32:02 +0100

    Unroll ifs for index compile (#29)

--- lib/expo/plural_forms/evaluator.ex
+++ lib/expo/plural_forms/evaluator.ex
@@ -3,6 +3,41 @@ defmodule Expo.PluralForms.Evaluator do
 
   alias Expo.PluralForms
 
+  @boolean_operators [:!=, :>, :<, :==, :>=, :<=, :&&, :||]
+
+  defmodule IntegerOperators do
+    @moduledoc false
+
+    # credo:disable-for-lines:28 Credo.Check.Readability.Specs
+    # credo:disable-for-lines:27 Credo.Check.Readability.FunctionNames
+
+    def left != right when Kernel.!=(left, right), do: 1
+    def _left != _right, do: 0
+
+    def left == right when Kernel.==(left, right), do: 1
+    def _left == _right, do: 0
+
+    def left > right when Kernel.>(left, right), do: 1
+    def _left > _right, do: 0
+
+    def left < right when Kernel.<(left, right), do: 1
+    def _left < _right, do: 0
+
+    def left <= right when Kernel.<=(left, right), do: 1
+    def _left <= _right, do: 0
+
+    def left >= right when Kernel.>=(left, right), do: 1
+    def _left >= _right, do: 0
+
+    # credo:disable-for-next-line Credo.Check.Warning.BoolOperationOnSameValues
+    def 1 && 1, do: 1
+    def _left && _right, do: 0
+
+    def 1 || _right, do: 1
+    def _left || 1, do: 1
+    def _left || _right, do: 0
+  end
+
   @spec index(plural_forms :: PluralForms.plural(), n :: non_neg_integer()) :: non_neg_integer()
   def index(plural, n)
   def index(:n, n) when is_integer(n), do: n
@@ -13,108 +48,96 @@ defmodule Expo.PluralForms.Evaluator do
 
   def index({:paren, content}, n), do: index(content, n)
 
-  def index({:!=, left, right}, n), do: if(index(left, n) != index(right, n), do: 1, else: 0)
-  def index({:>, left, right}, n), do: if(index(left, n) > index(right, n), do: 1, else: 0)
-  def index({:<, left, right}, n), do: if(index(left, n) < index(right, n), do: 1, else: 0)
-  def index({:==, left, right}, n), do: if(index(left, n) == index(right, n), do: 1, else: 0)
+  for operator <- @boolean_operators do
+    def index({unquote(operator), left, right}, n),
+      do: IntegerOperators.unquote(operator)(index(left, n), index(right, n))
+  end
+
   def index({:%, left, right}, n), do: rem(index(left, n), index(right, n))
-  def index({:>=, left, right}, n), do: if(index(left, n) >= index(right, n), do: 1, else: 0)
-  def index({:<=, left, right}, n), do: if(index(left, n) <= index(right, n), do: 1, else: 0)
-
-  def index({:&&, left, right}, n),
-    do: if(index(left, n) == 1 and index(right, n) == 1, do: 1, else: 0)
-
-  def index({:||, left, right}, n),
-    do: if(index(left, n) == 1 or index(right, n) == 1, do: 1, else: 0)
 
   @spec compile_index(plural_forms :: PluralForms.plural()) :: Macro.t()
-  def compile_index(plural)
+  def compile_index(plural) do
+    plural
+    |> unroll_nested_ifs_to_cond()
+    |> _compile_index()
+  end
 
-  def compile_index(:n) do
+  defp unroll_nested_ifs_to_cond(plural)
+
+  defp unroll_nested_ifs_to_cond({:if, condition, truthy, falsy}) do
+    condition = unroll_nested_ifs_to_cond(condition)
+    truthy = unroll_nested_ifs_to_cond(truthy)
+    falsy = unroll_nested_ifs_to_cond(falsy)
+
+    # Only the false branch needs unrolling since only that is actually used
+    conditions =
+      case falsy do
+        {:cond, child_conditions} -> [{condition, truthy} | child_conditions]
+        other -> [{condition, truthy}, {1, other}]
+      end
+
+    {:cond, conditions}
+  end
+
+  defp unroll_nested_ifs_to_cond({:paren, plural}),
+    do: {:paren, unroll_nested_ifs_to_cond(plural)}
+
+  defp unroll_nested_ifs_to_cond({operator, left, right})
+       when operator in [:!=, :>, :<, :==, :%, :>=, :<=, :&&, :||],
+       do: {operator, unroll_nested_ifs_to_cond(left), unroll_nested_ifs_to_cond(right)}
+
+  defp unroll_nested_ifs_to_cond(:n), do: :n
+  defp unroll_nested_ifs_to_cond(number) when is_integer(number), do: number
+
+  defp _compile_index(plural)
+
+  defp _compile_index(:n) do
     quote do
       var!(n)
     end
   end
 
-  def compile_index(number) when is_integer(number), do: number
+  defp _compile_index(number) when is_integer(number), do: number
 
-  def compile_index({:if, condition, truthy, falsy}) do
+  defp _compile_index({:cond, conditions}) do
+    conditions =
+      Enum.map(conditions, fn
+        {1, result} ->
+          {:->, [], [[true], _compile_index(result)]}
+
+        {condition, result} ->
+          {:->, [],
+           [
+             [
+               quote do
+                 Kernel.==(unquote(_compile_index(condition)), 1)
+               end
+             ],
+             _compile_index(result)
+           ]}
+      end)
+
     quote do
-      if unquote(compile_index(condition)) == 1,
-        do: unquote(compile_index(truthy)),
-        else: unquote(compile_index(falsy))
+      cond do: unquote(conditions)
     end
   end
 
-  def compile_index({:paren, content}), do: compile_index(content)
+  defp _compile_index({:paren, content}), do: _compile_index(content)
 
-  def compile_index({:!=, left, right}) do
+  defp _compile_index({:%, left, right}) do
     quote do
-      if unquote(compile_index(left)) != unquote(compile_index(right)),
-        do: 1,
-        else: 0
+      rem(unquote(_compile_index(left)), unquote(_compile_index(right)))
     end
   end
 
-  def compile_index({:>, left, right}) do
-    quote do
-      if unquote(compile_index(left)) > unquote(compile_index(right)),
-        do: 1,
-        else: 0
-    end
-  end
-
-  def compile_index({:<, left, right}) do
-    quote do
-      if unquote(compile_index(left)) < unquote(compile_index(right)),
-        do: 1,
-        else: 0
-    end
-  end
-
-  def compile_index({:==, left, right}) do
-    quote do
-      if unquote(compile_index(left)) == unquote(compile_index(right)),
-        do: 1,
-        else: 0
-    end
-  end
-
-  def compile_index({:%, left, right}) do
-    quote do
-      rem(unquote(compile_index(left)), unquote(compile_index(right)))
-    end
-  end
-
-  def compile_index({:>=, left, right}) do
-    quote do
-      if unquote(compile_index(left)) >= unquote(compile_index(right)),
-        do: 1,
-        else: 0
-    end
-  end
-
-  def compile_index({:<=, left, right}) do
-    quote do
-      if unquote(compile_index(left)) <= unquote(compile_index(right)),
-        do: 1,
-        else: 0
-    end
-  end
-
-  def compile_index({:&&, left, right}) do
-    quote do
-      if unquote(compile_index(left)) == 1 and unquote(compile_index(right)) == 1,
-        do: 1,
-        else: 0
-    end
-  end
-
-  def compile_index({:||, left, right}) do
-    quote do
-      if unquote(compile_index(left)) == 1 or unquote(compile_index(right)) == 1,
-        do: 1,
-        else: 0
+  for operator <- @boolean_operators do
+    defp _compile_index({unquote(operator) = operator, left, right}) do
+      quote do
+        IntegerOperators.unquote(operator)(
+          unquote(_compile_index(left)),
+          unquote(_compile_index(right))
+        )
+      end
     end
   end
 end
--- test/expo/plural_forms_test.exs
+++ test/expo/plural_forms_test.exs
@@ -545,8 +545,8 @@ defmodule Expo.PluralFormsTest do
       assert {:ok, {6, plurals}} =
                PluralForms.parse_string("""
                nplurals=6; \
-                     plural=n==0 ? 0 : n==1 ? 1 : n==2 ? 2 : n%100>=3 && n%100<=10 ? 3 \
-                     : n%100>=11 ? 4 : 5;
+                     plural=(n==0 ? 0 : n==1 ? 1 : n==2 ? 2 : n%100>=3 && n%100<=10 ? 3 \
+                     : n%100>=11 ? 4 : 5);
                """)
 
       quoted = PluralForms.compile_index(plurals)
